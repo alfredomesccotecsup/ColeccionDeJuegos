990951fdd0613ebfdd45ff153f092597e00b78c6
Alfredo <rosendo.mescco@tecsup.edu.pe>
2023-10-24 16:18:20 -0500

    Initial Commit

---
+++ MesccoColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MesccoColeccionDeJuegos
+//
+//  Created by Mescco Quiespe, Rosendo Alfredo on 24/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
